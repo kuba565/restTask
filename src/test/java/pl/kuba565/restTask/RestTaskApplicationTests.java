51f5af6297ce5b51ded4b81ca1cf9c9ef286e061
Jakub Kąkolewski <jkakolewski@sii.pl>
2019-08-05 16:30:02 +0200

    first commit

---
+++ src/test/java/pl/kuba565/restTask/RestTaskApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.kuba565.restTask;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestTaskApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
