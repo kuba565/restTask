28f9a354aec40e5a15e50cecdcb7733e6422d699
Jakub Kąkolewski <jkakolewski@sii.pl>
2019-09-19 13:43:32 +0200

    added profile 'jooq' property to switch to jooq rrepository config

--- src/main/java/pl/kuba565/resttask/service/JooqServiceConfig.java
+++ src/main/java/pl/kuba565/resttask/service/JooqServiceConfig.java
@@ -3,21 +3,22 @@ package pl.kuba565.resttask.service;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import pl.kuba565.resttask.repository.CarRepositoryImpl;
-import pl.kuba565.resttask.repository.WorkerRepositoryImpl;
+import pl.kuba565.resttask.repository.JooqCarRepositoryImpl;
+import pl.kuba565.resttask.repository.JooqWorkerRepositoryImpl;
 import pl.kuba565.resttask.validator.CarValidator;
 import pl.kuba565.resttask.validator.WorkerValidator;
 
 @Configuration
-@Profile("!jooq")
-public class ServiceConfig {
+@Profile("jooq")
+public class JooqServiceConfig {
+
     @Bean
-    public CarServiceImpl carService(CarRepositoryImpl carRepositoryImpl, CarValidator carValidator) {
+    public CarServiceImpl carService(JooqCarRepositoryImpl carRepositoryImpl, CarValidator carValidator) {
         return new CarServiceImpl(carRepositoryImpl, carValidator);
     }
 
     @Bean
-    public WorkerServiceImpl workerService(WorkerRepositoryImpl workerRepositoryImpl, WorkerValidator workerValidator) {
+    public WorkerServiceImpl workerService(JooqWorkerRepositoryImpl workerRepositoryImpl, WorkerValidator workerValidator) {
         return new WorkerServiceImpl(workerRepositoryImpl, workerValidator);
     }
 }
